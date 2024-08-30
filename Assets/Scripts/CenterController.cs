0b60924ec5b76413d84793d82561aa4a1c72cc05
TeemuUusitalo1 <s2200914@edu.bc.fi>
2024-08-30 12:57:32 +0300

    Some assets and movement start

---
+++ Assets/Scripts/CenterController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterController : MonoBehaviour
+{
+    [SerializeField] Transform[] Points;
+
+    [SerializeField] private float moveSpeed;
+
+    private int pointsIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+        transform.position = Points[pointsIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moving "Center" Between Points (L & R) when pressing a or d
+        if(pointsIndex <= Points.Length -1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
+            if (transform.position == Points[pointsIndex].transform.position)
+            {
+                pointsIndex += 1;
+            }
+        }
+        //Input.GetKeyDown(KeyCode.A)
+    }
+}
