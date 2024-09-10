f34b849d70e354d4d8d17ccf250a44bc953c9c08
TeemuUusitalo1 <s2200914@edu.bc.fi>
2024-09-10 11:53:39 +0300

    Ball Spawning (Incomplete)

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -6,42 +6,51 @@ public class BallSpawner : MonoBehaviour
 {
 
     [SerializeField] Transform[] Points;
-    private int pointsIndex;
-    private float point = 1f;
-    private float point2 = 1f;
-    private float point3 = 1f;
+    private int Spawnpoints;
+    private int Objects;
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
     public GameObject myPrefab;
     public GameObject Prefab2;
     // Start is called before the first frame update
     void Start()
     {
-        point = pointsIndex;
-        point2 = pointsIndex;
-        point3 = pointsIndex;
+
+        //Randomize object
+
+        SpawnBall();
 
         // Vector3 firstPos = positions[firstPosIdx];
         // Vector3 secondPos = positions[1 - firstPosIdx];
 
-        int firstPosIdx = Random.Range(0, 1);
+       // int firstPosIdx = Random.Range(0, 1);
 
+    }
+
+    private void SpawnBall()
+    {
+        int Objects = 0;
+        int RandomColor = Random.Range(0, 3);
+        int randomChance = Random.Range(0, 22);
+        while (Objects < 20)
+        {
+            if (randomChance == 1 && RandomColor == 1)
+            {
+                // Instantiate at position
+                Instantiate(myPrefab, new Vector3(20, 24, 22), Quaternion.identity);
+                Objects = +1;
+            }
+            if (randomChance >= 2 && RandomColor == 2)
+            {
+                Instantiate(Prefab2, new Vector3(19, 24, 22), Quaternion.identity);
+                Objects = +1;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Randomize object
-        float randomChance = Random.Range(0.0f, 1.0f);
-        if (randomChance < 0.4f)
-        {
-            // Instantiate at position
-            Instantiate(myPrefab, new Vector3(point2, point3, point), Quaternion.identity);
-        }
-        else
-        {
-            Instantiate(Prefab2, new Vector3(19, 24, 18), Quaternion.identity);
 
-        }
     }
 
 
